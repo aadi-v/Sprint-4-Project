e15e7f89b824f6fb4f22aa7357f7da2c89ffc3fe
aadi-v <aadivyas4@gmail.com>
2024-07-30 19:56:28 -0400

    2nd change

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Sprint-4-Project
 Tripleten Sprint 4 Project
+
+This project does a basic EDA on car vehicle sales in the US and is visualized in a few simple graphs.
+
+The EDA results are then displayed in Streamlit.
--- app.py
+++ app.py
@@ -4,4 +4,17 @@ import plotly.express as px
 
 vehicles = pd.read_csv('vehicles_us.csv')
 
-st.header('United States Vehicle Data')
+title1 = st.checkbox('Title 1')
+title2 = st.checkbox('Title 2')
+
+if title1:
+    st.header('United States Vehicle Data', divider='blue')
+if title2
+    st.header('US Vehicle Data (1908-2019)', divder='red')
+
+st.header('United States Vehicle Data', divider='blue')
+
+chart1 = px.scatter(vehicles, x='model_year', y='price')
+chart2 = px.histogram(vehicles['model_year'])
+st.write(chart1)
+st.write(chart2)
