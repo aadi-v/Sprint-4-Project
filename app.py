3820444310c41a92c63db6b428f2c8e9b451c714
aadi-v <aadivyas4@gmail.com>
2024-07-31 11:26:41 -0400

    first project revision based on reviewer feedback

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import plotly.express as px
 
 vehicles = pd.read_csv('vehicles_us.csv')
+vehicles_50 = vehicles_cyls[vehicles_cyls['price']<=9000]
+vehicles_75 = vehicles_cyls[vehicles_cyls['price']<=17000]
 
 st.write('Header Selection:')
 title1 = st.checkbox('Header 1')
@@ -16,7 +18,21 @@ if title2:
 st.write('Student note: how can I use streamlit checkboxes to modify graphs? I could not find any good examples online.')
 st.write('Would it be possible to have examples or lessons sent to me?')
 
-chart1 = px.scatter(vehicles, x='model_year', y='price', title='Car Model Year vs Price',)
-chart2 = px.histogram(vehicles['model_year'], title='Model Year vs Frequency')
+chart1 = px.scatter(vehicles, x='model_year', y='odometer', title='Car Model Year vs Mileage')
+chart2 = px.histogram(vehicles_50['price'], x='price', title='Price vs Frequency')
+chart3 = px.histogram(vehicles_75['price'], x='price', title='Price vs Frequency')
+chart4 = px.histogram(vehicles['price'], x='price', title='Price vs Frequency')
+
 st.write(chart1)
-st.write(chart2)
+
+st.write('Histogram Selection:')
+hist1 = st.checkbox('50th Percentile of Car Prices')
+hist2 = st.checkbox('75th Percentile of Car Prices')
+hist3 = st.checkbox('All Car Prices')
+
+if hist1:
+    st.write(chart2)
+if hist2:
+    st.write(chart3)
+if hist3:
+    st.write(chart4)
